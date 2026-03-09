6407c49eefd3314dd4abb1b860cafcad17703717
Anirban chattaraj <anirbanchattaraj@Anirbans-MacBook-Air.local>
2026-03-09 13:19:45 +0530

    Added palindrome array program

---
+++ Q15.c
@@ -0,0 +1,38 @@
+//Check if array is palindrome
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter your number: ");
+   scanf("%d",&n);
+
+   int arr[n];
+
+   printf("Enter your elements:\n");
+
+   for (int i = 0; i < n; i++)
+   {
+      scanf("%d",&arr[i]);
+   }
+
+   int flag = 1;
+
+   for (int i = 0; i < n/2; i++)
+   {
+      if (arr[i] != arr[n-i-1])
+      {
+         flag = 0;
+         break;
+      }
+   }
+
+   if (flag == 1)
+   {
+      printf("Array is Palindrome");
+   }
+   else
+   {
+      printf("Array is not Palindrome");
+   }
+
+   return 0;
+}
